e2fa63af4eb2c327d62dc53ed6ab326dc6dec22d
Andoniaina <amrazanadrakoto@gmail.com>
2025-07-07 14:34:25 +0200

    activer menu sur mobile

--- starterOnly/main.js
+++ starterOnly/main.js
@@ -1,11 +1,3 @@
-function adjustBground() {
-  const topnav = document.getElementById("myTopnav");
-  const bground = document.querySelector(".bground");
-  const navHeight = topnav.offsetHeight;
-  bground.style.top = navHeight + "px";
-  bground.style.height = `calc(100% - ${navHeight}px)`;
-}
-
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -13,24 +5,26 @@ function editNav() {
   } else {
     x.className = "topnav";
   }
-  adjustBground(); // update bground position after toggling nav
 }
 
-/* Prise en charge de la modal *********************************************************/
-
-const closeModalIcon = document.getElementById("closeModal");
-closeModalIcon.addEventListener("click", closeModal);
-
+/* Prise en charge de la modal */
 const topnav = document.querySelector("#myTopnav")
 const heroSection = document.querySelector("#myHero-section")
 const modal = document.querySelector("#modal");
 const form = document.querySelector("form[name='reserve']");
-const confirmation = document.querySelector("#confirmation");
-const reserveForm = document.querySelector("form[name='reserve']");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-let errors = document.querySelectorAll('.text-error');
+
+//Afficher les menus lors du clique sur l'icon fa-bars
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("menu-button");
+  const mainNavbar = document.querySelector(".main-navbar");
+
+  menuButton.addEventListener("click", () => {
+    mainNavbar.classList.toggle("active");
+  });
+});
 
 // launch modal form
 function launchModal() {
@@ -54,69 +48,51 @@ function launchModal() {
   document.getElementById("myFooter").style.display = "none";
 }
 
-//Fermer les modal avec close
-function closeModal() {
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Affichage du page d'acceuil au rafraichissement
+window.addEventListener("DOMContentLoaded", () => {
+  // Cacher la modal au chargement
+  const modal = document.getElementById("modal");
+  const form = document.querySelector("form[name='reserve']");
+  const confirmation = document.getElementById("confirmation");
+
   modal.style.display = "none";
+  form.style.display = "block"; // Assure que le formulaire est prêt mais caché
   confirmation.style.display = "none";
-  topnav.style.display = "block"
-  heroSection.style.display = "";
+
+  // Réafficher la page d'accueil
+  document.getElementById("myHero-section").style.display = "block";
   document.getElementById("myFooter").style.display = "block";
-}
+  document.getElementById("myTopnav").style.display = "block";
+});
 
-function submitForm() {
-  if (validate()) {
-    // Masquer le formulaire
-    form.style.display = "none";
-    // Réinitialiser le formulaire
-    reserveForm.reset();
-    // Afficher le modal de confirmation
-    confirmation.style.display = "block";
-  }
-}
-
-/**************************************************************************************/
 // Etape 2 - Cacher l'erreur
+let errors = document.querySelectorAll('.text-error');
 errors.forEach(function (error) {
   error.style.display = "none";
 });
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // Ajout des écouteurs une seule fois
-document.getElementById("first").addEventListener("input", validateFirst);
-document.getElementById("last").addEventListener("input", validateLast);
+document.getElementById("first").addEventListener("input", () => validateInput("first"));
+document.getElementById("last").addEventListener("input", () => validateInput("last"));
 document.getElementById("email").addEventListener("input", validateEmail);
 document.getElementById("birthdate").addEventListener("input", validateBirthdate);
 document.getElementById("quantity").addEventListener("input", validateQuantity);
 
 // Récupération et vérifier les champs à valider
-// Vérification Prénom
-function validateFirst() {
-  const first = document.getElementById("first");
-  const firstError = first.parentElement.querySelector(".text-error");
-  if (first.value.trim().length < 2) {
-    first.classList.add('input-error');
-    firstError.style.display = "inline";
+// Vérification Prénom et nom
+function validateInput(id) {
+  const input = document.getElementById(id);
+  const error = input.parentElement.querySelector(".text-error");
+  if (input.value.trim().length < 2) {
+    input.classList.add('input-error');
+    error.style.display = "inline";
     return false;
   } else {
-    first.classList.remove('input-error');
-    firstError.style.display = "none";
-    return true;
-  }
-}
-
-// Vérifier nom
-function validateLast() {
-  const last = document.getElementById("last");
-  const lastError = last.parentElement.querySelector(".text-error");
-  if (last.value.trim().length < 2) {
-    last.classList.add('input-error');
-    lastError.style.display = "inline";
-    return false;
-  } else {
-    last.classList.remove('input-error');
-    lastError.style.display = "none";
+    input.classList.remove('input-error');
+    error.style.display = "none";
     return true;
   }
 }
@@ -201,18 +177,38 @@ function validatecheckbox1() {
   }
 }
 
-// Validation au submit
-function validate() {
-  let valid = true;
-  if (!validateFirst()) valid = false;
-  if (!validateLast()) valid = false;
-  if (!validateEmail()) valid = false;
-  if (!validateBirthdate()) valid = false;
-  if (!validateQuantity()) valid = false;
-  if (!validateLocation()) valid = false;
-  if (!validatecheckbox1()) valid = false;
-  return valid; // Si false, le submit est bloqué
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.querySelector(".btn-submit");
+  const form = document.querySelector("form[name='reserve']");
+  const reserveForm = document.querySelector("form[name='reserve']");
+  const confirmation = document.querySelector("#confirmation");
+
+  submitBtn.addEventListener("click", submitForm);
+
+  function submitForm() {
+    if (validate()) {
+      // Masquer le formulaire
+      form.style.display = "none";
+      // Réinitialiser le formulaire
+      reserveForm.reset();
+      // Afficher le modal de confirmation
+      confirmation.style.display = "block";
+    }
+  }
+
+  // Validation au submit
+  function validate() {
+    let valid = true;
+    if (!validateInput("first")) valid = false;
+    if (!validateInput("last")) valid = false;
+    if (!validateEmail()) valid = false;
+    if (!validateBirthdate()) valid = false;
+    if (!validateQuantity()) valid = false;
+    if (!validateLocation()) valid = false;
+    if (!validatecheckbox1()) valid = false;
+    return valid; // Si false, le submit est bloqué
+  }
+});
 
 // Fermer le message de confirmation
 function close() {
@@ -226,12 +222,18 @@ function close() {
   document.getElementById("myFooter").style.display = "block";
 }
 
-const menuButton = document.getElementById('menu-button');
-const navbar = document.querySelector('.main-navbar');
+const closeModalIcon = document.getElementById("closeModal");
+closeModalIcon.addEventListener("click", closeModal);
+//Fermer les modal avec close
+function closeModal() {
+  modal.style.display = "none";
+  confirmation.style.display = "none";
+  topnav.style.display = "block"
+  heroSection.style.display = "";
+  document.getElementById("myFooter").style.display = "block";
+}
 
-menuButton.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
+
 
 
 
